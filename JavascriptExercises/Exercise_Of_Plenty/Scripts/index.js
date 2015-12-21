0c0fd1e273f925cccd52c1437f6fb5c41d4ec64e
Montagu-Evans <montagu-evans@hotmail.com>
2015-12-21 10:28:21 +0100

    Skapat funktionen för att räkna ut månadslönen

--- JavascriptExercises/Exercise_Of_Plenty/Scripts/index.js
+++ JavascriptExercises/Exercise_Of_Plenty/Scripts/index.js
@@ -2,3 +2,10 @@
 var månadslön = document.getElementById('månadslön').innerHTML;
 var lön = document.getElementById('lön')
     .addEventListener('click', Calculate, false);
+
+
+function Calculate() {
+    var försäljning = document.getElementById('försäljningssumma').value * 0.091;
+
+    document.getElementById('lön').innerHTML = Number(månadslön) + Number(försäljning);
+}
