e1972f5422e3d1f30dba51d93135c2c580b8d461
Montagu-Evans <montagu-evans@hotmail.com>
2015-12-21 11:02:19 +0100

    Gjort en funktion för sekunder

--- JavascriptExercises/Exercise_Of_Plenty/Scripts/index2.js
+++ JavascriptExercises/Exercise_Of_Plenty/Scripts/index2.js
@@ -1,7 +1,14 @@
 ﻿
 var resultatminuter = document.getElementById('resultat-minuter').addEventListener('click', CalculateMinutes, false);
 
+var resultatsekunder = document.getElementById('resultat-sekunder').addEventListener('click', CalculateSeconds, false);
+
 function CalculateMinutes() {
     var antalminuter = document.getElementById('antaltimmar').value * 60;
     document.getElementById('resultat-minuter').innerHTML = Number(antalminuter);
+}
+
+function CalculateSeconds() {
+    var antalsekunder = document.getElementById('antaltimmar').value * 60 * 60;
+    document.getElementById('resultat-sekunder').innerHTML = Number(antalsekunder);
 }
