099ed373ecf46f4ed54e6eab24ce1591b4d954a3
Montagu-Evans <montagu-evans@hotmail.com>
2015-12-21 12:57:38 +0100

    Gjort en funktion som skriver ut sqrten på ett tal

---
+++ JavascriptExercises/Exercise_Of_Plenty/Scripts/index4.js
@@ -0,0 +1,8 @@
+﻿
+
+var resultat = document.getElementById('resultat').addEventListener('mouseover', SquareRoot, false);
+
+function SquareRoot() {
+    var tal = document.getElementById('tal').value;
+    return document.getElementById('resultat').innerHTML = Math.sqrt(tal);
+}
