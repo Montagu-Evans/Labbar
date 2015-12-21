4aba4846303497164f7fbcbb70c5b2cbae9d705e
Montagu-Evans <montagu-evans@hotmail.com>
2015-12-21 10:48:04 +0100

    Gjort en funktion som skriver ut timmar i minuter

---
+++ JavascriptExercises/Exercise_Of_Plenty/Scripts/index2.js
@@ -0,0 +1,7 @@
+﻿
+var resultatminuter = document.getElementById('resultat-minuter').addEventListener('click', CalculateMinutes, false);
+
+function CalculateMinutes() {
+    var antalminuter = document.getElementById('antaltimmar').value * 60;
+    document.getElementById('resultat-minuter').innerHTML = Number(antalminuter);
+}
