dc9c8267b16bff1061192ac2aa4052e132d13c7e
Montagu-Evans <montagu-evans@hotmail.com>
2015-12-21 12:34:07 +0100

    Lagt till en funktion som skriver ut medelvärdet av de tre inskrivna talen

--- JavascriptExercises/Exercise_Of_Plenty/Scripts/index3.js
+++ JavascriptExercises/Exercise_Of_Plenty/Scripts/index3.js
@@ -1,11 +1,21 @@
 ﻿
-
 var summa = document.getElementById('summa').addEventListener('click', Add, false);
+
+var average = document.getElementById('medelvärde').addEventListener('click', Average, false);
 
 function Add() {
     var talett = document.getElementById('tal-ett').value;
     var taltvå = document.getElementById('tal-två').value;
     var taltre = document.getElementById('tal-tre').value;
-    
+
     return document.getElementById('summa').innerHTML = Number(talett) + Number(taltvå) + Number(taltre);
-} 
+}
+
+function Average() {
+    var talett = document.getElementById('tal-ett').value;
+    var taltvå = document.getElementById('tal-två').value;
+    var taltre = document.getElementById('tal-tre').value;
+
+    return document.getElementById('medelvärde').innerHTML = (Number(talett) + Number(taltvå) + Number(taltre)) / 3;
+}
+
