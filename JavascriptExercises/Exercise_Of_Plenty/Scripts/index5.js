0965808873e4f96cb8faea20d8b31f8557412621
Montagu-Evans <montagu-evans@hotmail.com>
2015-12-21 13:44:30 +0100

    Gjort färdigt Task5

---
+++ JavascriptExercises/Exercise_Of_Plenty/Scripts/index5.js
@@ -0,0 +1,14 @@
+﻿
+var convert_to_pounds = document.getElementById('pounds').addEventListener('click', ConvertToPounds, false);
+
+var convert_to_dollars = document.getElementById('dollars').addEventListener('click', ConvertToDollars, false);
+
+function ConvertToPounds() {
+    var amount = document.getElementById('amount').value;
+    return document.getElementById('pounds').innerHTML = (Number(amount) * 12);
+}
+
+function ConvertToDollars() {
+    var amount = document.getElementById('amount').value;
+    return document.getElementById('dollars').innerHTML = (Number(amount) * 6);
+}
